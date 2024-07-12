c98f1222e5a8d49b1991d905231230a355a2e00d
Ivan-Korotkov-63-AMG <ivan.korotkov.05.com@gmail.com>
2024-07-12 18:11:10 +0400

    Update week.js

--- js/weather/week.js
+++ js/weather/week.js
@@ -1,6 +1,6 @@
 const apiKey1 = '9fe4014855734c44b4394202240407';
 const city = 'Sarapul';
-const url = `https://api.weatherapi.com/v1/forecast.json?key.json?key=${apiKey}&q=${city}&days=7&aqi=no&alerts=no`;
+const url = `https://api.weatherapi.com/v1/forecast.json?key.json?key=${apiKey1}&q=${city}&days=7&aqi=no&alerts=no`;
 
 
 fetch('url')
